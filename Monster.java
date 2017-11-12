7c4c6a6939591bea7fc35a72865a49f92fa4f460
eddiezhu3 <33487778+eddiezhu3@users.noreply.github.com>
2017-11-12 14:21:59 -0500

    Add files via upload

--- Monster.java
+++ Monster.java
@@ -3,18 +3,10 @@ Tania Cao, Jerry Ye, Eddie Zhu -- Monster
 APCS1 pd1
 HW#28
 2017-11-08
-* class YoRPG -- Driver file for Ye Olde Role Playing Game.
-* Simulates monster encounters of a wandering adventurer.
-* Required classes: Protagonist, Monster
+
 **********************************************/
 
-public class Monster{
-    //instance variables
-    public String name;
-    public int health;
-    public int strength;
-    public int defense;
-    public double attackRating;
+public class Monster extends Character{
 
     //constructor
     public Monster(){
@@ -22,31 +14,6 @@ public class Monster{
 	defense = 20;
 	attackRating = 1;
 	strength = (int)(Math.random() * 45) + 20;
-	String name = "smaug";
     }
-
-    //returns true if health is above 0
-    public boolean isAlive(){
-	return (health > 0);
-    }
-    //returns the defense amount
-    public int getDefense(){
-	return defense;
-    }
-    //returns the name
-    public String getName(){
-	return name;
-    }
-    //reduces health by the damage taken
-    public int lowerHP(int damageTaken){
-	return (health - damageTaken);
-    }
-
-    public int attack(Protagonist pat){
-	int damageDone = (int)(strength * attackRating) - pat.getDefense();
-	pat.health -= damageDone;
-	return damageDone;
-    }
-
     
 }
--- Protagonist.java
+++ Protagonist.java
@@ -3,18 +3,11 @@ Tania Cao, Jerry Ye, Eddie Zhu -- Monster
 APCS1 pd1
 HW#28
 2017-11-08
-* class YoRPG -- Driver file for Ye Olde Role Playing Game.
-* Simulates monster encounters of a wandering adventurer.
-* Required classes: Protagonist, Monster
+
 **********************************************/
 
-public class Protagonist{
-    //instance variables
-    public String name;
-    public int health;
-    public int strength;
-    public int defense;
-    public double attackRating;
+public class Protagonist extends Character{
+
 
     //constructor
     public Protagonist(String newName){
@@ -23,29 +16,6 @@ public class Protagonist{
 	strength = 100;
 	defense = 40;
 	attackRating = 0.4;
-    }
-
-    //returns true if health is above 0
-    public boolean isAlive(){
-	return (health > 0);
-    }
-    //returns the defense amount
-    public int getDefense(){
-	return defense;
-    }
-    //returns the name
-    public String getName(){
-	return name;
-    }
-    //reduces health by the damage taken
-    public int lowerHP(int damageTaken){
-	return (health - damageTaken);
-    }
-    //returns the damage done to the monster based on formula
-    public int attack(Monster smaug){
-	int damageDone = (int)(strength * attackRating) - smaug.getDefense();
-	smaug.health -= damageDone;
-	return damageDone;
     }
     //decrease defense, increase attackRating
     public void specialize(){
@@ -56,7 +26,6 @@ public class Protagonist{
     public void normalize(){
 	defense = 40;
 	attackRating = .4;
-    }
 	   
     
-}
+    }
